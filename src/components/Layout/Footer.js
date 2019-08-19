34d5beef662ce8fc602c2fef441751fb298bc352
Eduardo <eduardo.y.t@hotmail.com>
2019-08-19 20:58:03 -0300

    primer commit para deploy

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -6,73 +6,7 @@ export default function Footer() {
         <div className="main-footer">
       <div className="footer-middle">
         <div className="container">
-          <div className="row">
-            {/* Column 1 */}
-            <div className="col-md-3 col-sm-6">
-              <h4>Lorem ipsum</h4>
-              <ul className="list-unstyled">
-                <li>Lorem ipsum</li>
-                <li>Lorem ipsum</li>
-                <li>Lorem ipsum</li>
-                <li>Lorem ipsum</li>
-              </ul>
-            </div>
-            {/* Column 2 */}
-            <div className="col-md-3 col-sm-6">
-              <h4>Lorem ipsum</h4>
-              <ul className="list-unstyled">
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-              </ul>
-            </div>
-            {/* Column 3 */}
-            <div className="col-md-3 col-sm-6">
-              <h4>Lorem ipsum</h4>
-              <ul className="list-unstyled">
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-              </ul>
-            </div>
-            {/* Column 4 */}
-            <div className="col-md-3 col-sm-6">
-              <h4>Lorem ipsum</h4>
-              <ul className="list-unstyled">
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-                <li>
-                  <a href="#">Lorem ipsum</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          {/* Footer Bottom */}
+
           <div className="footer-bottom">
             <p className="text-xs-center">
               &copy;{new Date().getFullYear()} City Guide App - All Rights

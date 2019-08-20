78c8beb8aaaa468fc5e1ee284bca2ed781719146
Eduardo Yujra <eduardo.yujra@snoopconsulting.com>
2019-08-20 14:33:14 -0300

    agreagar descripcion para miembros

--- src/components/Pages/Miembros.js
+++ src/components/Pages/Miembros.js
@@ -15,9 +15,9 @@ export default function Miembros() {
                     <div className="card" >
                         <img src={gonza} className="card-img-top" alt="..."></img>
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title">Gonzalo</h5>
+                            <p className="card-text">ma niga</p>
+                            <a href="#" className="btn btn-primary">Go to the hell</a>
                         </div>
                     </div>
                 </div>
@@ -26,9 +26,9 @@ export default function Miembros() {
                     <div className="card" >
                         <img src={kev} className="card-img-top" alt="..."></img>
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title">Kevin</h5>
+                            <p className="card-text">Graso's bitch</p>
+                            <a href="#" className="btn btn-primary">Go to "maldito perro del infierno"</a>
                         </div>
                     </div>
                 </div>
@@ -37,9 +37,9 @@ export default function Miembros() {
                     <div className="card" >
                         <img src={fran} className="card-img-top" alt="..."></img>
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title">Franco</h5>
+                            <p className="card-text">Bald</p>
+                            <a href="#" className="btn btn-primary">Go Tio Nacho</a>
                         </div>
                     </div>
                 </div>
@@ -48,9 +48,9 @@ export default function Miembros() {
                     <div className="card" >
                         <img src={edu} className="card-img-top" alt="..."></img>
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title">Eduardo</h5>
+                            <p className="card-text">Death</p>
+                            <a href="#" className="btn btn-primary">KILL</a>
                         </div>
                     </div>
                 </div>
